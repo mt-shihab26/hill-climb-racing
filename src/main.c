05b04a2f2f436b7378e55f0fdb3608b5916b5892
Shihab Mahamud <shihab4t@gmail.com>
2025-06-27 10:18:57 +0600

    car and wheel

--- src/main.c
+++ src/main.c
@@ -1,14 +1,46 @@
 #include <raylib.h>
 
+typedef struct Car {
+    float x; // x position
+    float y; // y position
+    float w; // weight of car
+    float h; // height of car
+} Car;
+
+typedef struct Wheel {
+    float r; // radius
+    float p; // padding
+} Wheel;
+
 int main() {
     InitWindow(1000, 800, "Hill Clmb Racing");
 
     SetTargetFPS(60);
 
+    Car car = {
+        .x = 300,
+        .y = 300,
+        .w = 250,
+        .h = 100,
+    };
+
+    Wheel back_wheel = {
+        .r = 25,
+        .p = 10,
+    };
+
     while (!WindowShouldClose()) {
         BeginDrawing();
 
-        DrawCircle(100, 100, 10, WHITE);
+        ClearBackground(WHITE);
+
+        DrawRectangle(car.x, car.y, car.w, car.h, BLACK);
+        DrawCircle(car.x + back_wheel.r + back_wheel.p,
+                   car.y + car.h + back_wheel.r + back_wheel.p, back_wheel.r,
+                   BLACK);
+        DrawCircle(car.x + car.w - back_wheel.r - back_wheel.p,
+                   car.y + car.h + back_wheel.r + back_wheel.p, back_wheel.r,
+                   BLACK);
 
         EndDrawing();
     }
